c5c88e44606cd16ac5ecbb4996f9518cc32e5dc2
nielheo <daniel.hartono@agoda.com>
2019-06-10 18:27:04 +0700

    Balance, display system amount

--- screens/BalanceScreen/BalanceList.tsx
+++ screens/BalanceScreen/BalanceList.tsx
@@ -15,7 +15,7 @@ const BalanceList = (props: any) => {
             marginTop: 1,
             marginBottom: 5,
             marginLeft: 8,
-            color: '#999'
+            color: '#333'
           }}>{props.title}</Text>
       </View>
       <View style={{ flex: 0.5}}>
@@ -29,24 +29,45 @@ const BalanceList = (props: any) => {
       </View>
     </View>
     {props.accounts.map(account => (
-      <View style={{ flexDirection: 'row', flex: 1 }} key={idx++}>
-        <View style={{ flex: 0.5}}>
-          <Text style={{fontSize: 14,
-              marginTop: 1,
-              marginBottom: 5,
-              marginLeft: 15,
-              color: '#999'
-            }}>{account.name}</Text>
+      <React.Fragment  key={idx++}>
+        <View style={{ flexDirection: 'row', flex: 1 }} key={idx++}>
+          <View style={{ flex: 0.5}}>
+            <Text style={{fontSize: 14,
+                marginTop: 1,
+                marginBottom: 5,
+                marginLeft: 15,
+                color: '#333'
+              }}>{account.name}</Text>
+          </View>
+          <View style={{ flex: 0.5}}>
+            <Text style={{fontSize: 14,
+                marginTop: 1,
+                marginBottom: 5,
+                color: '#333',
+                textAlign: 'right'
+              }}>{account.currency} {numeral(account.localAmount).format('0,0.00')}</Text>
+          </View>
         </View>
-        <View style={{ flex: 0.5}}>
-          <Text style={{fontSize: 14,
-              marginTop: 1,
-              marginBottom: 5,
-              color: '#999',
-              textAlign: 'right'
-            }}>{account.currency} {numeral(account.localAmount).format('0,0.00')}</Text>
-        </View>
-      </View>
+        { props.currency != account.currency &&
+        <View style={{ flexDirection: 'row', flex: 1 }} key={'-' + idx}>
+          <View style={{ flex: 0.5}}>
+            <Text style={{fontSize: 14,
+                marginTop: 1,
+                marginBottom: 5,
+                marginLeft: 15,
+                color: '#A9A9A9'
+              }}></Text>
+          </View>
+          <View style={{ flex: 0.5}}>
+            <Text style={{fontSize: 12,
+                marginTop: 1,
+                marginBottom: 5,
+                color: '#A9A9A9',
+                textAlign: 'right'
+              }}>{props.currency} {numeral(account.amount).format('0,0.00')}</Text>
+          </View>
+        </View>}
+      </React.Fragment>
     ))}
   </React.Fragment>)
 }
